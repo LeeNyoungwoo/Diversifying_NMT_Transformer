81538a25d966a20f797b87513e1a5b2b1ded1298
root <passing2961@gmail.com>
2020-06-05 03:52:46 +0000

    add metric & sample_policy

---
+++ Transformer/metrics.py
@@ -0,0 +1,25 @@
+import numpy as np
+from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import SmoothingFunction
+
+def bleu_compute(reference, translation):
+    reference = reference.split()
+    translation = translation.split()
+    
+    return sentence_bleu([reference], translation, smoothing_function=SmoothingFunction().method7, weights=[1./3, 1./3, 1./3])
+
+def rfb_compute(bleu_score_list):
+    return sum(bleu_score_list) / len(bleu_score_list)
+
+def pwb_compute(translation_list):
+    pwb_score = []
+    for i in range(5):
+        for j in range(5):
+            if i == j:
+                continue
+            
+            pwb_score.append(bleu_compute(translation_list[i], translation_list[j]))
+    
+    assert len(pwb_score) == 20
+    
+    return sum(pwb_score) / len(pwb_score)
